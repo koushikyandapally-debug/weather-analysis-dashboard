acebd5da2cb23de5e3da6b562e2ef2a8bc02405a
koushik-codes <koushikyandapally@gmail.com>
2026-03-14 12:02:09 +0530

    Add weather fetching function in script.js
    Implement weather fetching functionality using OpenWeather API.

---
+++ script.js
@@ -0,0 +1,25 @@
+const apiKey = c3fe8d2da199a79c5eb203f3518aa342;
+
+function getWeather(){
+
+let city = document.getElementById("city").value;
+
+let url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+document.getElementById("").innerText = data.name;
+
+document.getElementById("temp").innerText = data.main.temp;
+
+document.getElementById("humidity").innerText = data.main.humidity;
+
+document.getElementById("wind").innerText = data.wind.speed;
+
+document.getElementById("weather").innerText = data.weather[0].main;
+
+});
+}
